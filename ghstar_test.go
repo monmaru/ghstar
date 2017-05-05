86a9a516600b7c2b894fdb42ab8943b5096e115c
Yoshitomo Minami <minami530@gmail.com>
2017-05-05 15:11:11 +0900

    Implemented unit tests

---
+++ ghstar_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/google/go-github/github"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsEmpty(t *testing.T) {
+	var tests = []struct {
+		in   string
+		want bool
+	}{
+		{"", true},
+		{"a", false},
+	}
+
+	for _, tt := range tests {
+		assert.Equal(t, isEmpty(tt.in), tt.want, "they should be equal")
+	}
+}
+
+func TestShow(t *testing.T) {
+	temp := "dummy"
+	repo := &github.Repository{HTMLURL: &temp}
+	show(repo)
+}
+
+func TestIsTargetLang(t *testing.T) {
+	lang := "Go"
+	var tests = []struct {
+		in1  string
+		in2  *string
+		want bool
+	}{
+		{"", nil, true},
+		{"", &lang, true},
+		{lang, &lang, true},
+		{strings.ToLower(lang), &lang, true},
+		{strings.ToUpper(lang), &lang, true},
+		{"goo", &lang, false},
+	}
+
+	for _, tt := range tests {
+		repo := &github.Repository{Language: tt.in2}
+		assert.Equal(t, isTargetLang(repo, tt.in1), tt.want, "they should be equal")
+	}
+}
--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
-	"strings"
 
-	"github.com/fatih/color"
-	"github.com/google/go-github/github"
 	"github.com/urfave/cli"
-	"golang.org/x/oauth2"
 )
 
 func main() {
@@ -52,75 +47,3 @@ func main() {
 	}
 	app.Run(os.Args)
 }
-
-type params struct {
-	lang, sort, direction string
-}
-
-func listRepositories(user string, params *params) error {
-	client := newGitHubClient()
-	opts := &github.ActivityListStarredOptions{
-		Sort:      params.sort,
-		Direction: params.direction,
-	}
-	opts.Page = 1
-	opts.PerPage = 50
-
-	for {
-		starredRepos, res, err := client.Activity.ListStarred(context.Background(), user, opts)
-		if err != nil {
-			return err
-		}
-
-		for _, r := range starredRepos {
-			repo := r.Repository
-			if isTargetLang(repo, params.lang) {
-				show(repo)
-			}
-		}
-
-		if res.NextPage == 0 {
-			return nil
-		}
-		opts.Page = res.NextPage
-	}
-}
-
-func newGitHubClient() *github.Client {
-	token := os.Getenv("GITHUB_API_TOKEN")
-	if isEmpty(token) {
-		return github.NewClient(nil)
-	}
-	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
-	return github.NewClient(oauth2.NewClient(context.Background(), ts))
-}
-
-func isTargetLang(r *github.Repository, lang string) bool {
-	if isEmpty(lang) {
-		return true
-	}
-	return (r.Language != nil) && (strings.ToLower(*r.Language) == strings.ToLower(lang))
-}
-
-func show(repo *github.Repository) {
-	fmt.Println("-----------------------------------------")
-	color.Green(*repo.HTMLURL)
-	if repo.Description != nil {
-		fmt.Println(*repo.Description)
-	}
-	if repo.Language != nil {
-		fmt.Printf("[Lang:%v] ", *repo.Language)
-	}
-	if repo.StargazersCount != nil {
-		fmt.Printf("[Star:%v] ", *repo.StargazersCount)
-	}
-	if repo.ForksCount != nil {
-		fmt.Printf("[Fork:%v] ", *repo.ForksCount)
-	}
-	if repo.UpdatedAt != nil {
-		fmt.Printf("[Updated at %v]", *repo.UpdatedAt)
-	}
-	fmt.Println("")
-}
-
-func isEmpty(s string) bool { return len(s) == 0 }
